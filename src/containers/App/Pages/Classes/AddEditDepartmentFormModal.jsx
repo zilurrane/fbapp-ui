d908c59c92d8502e69cf3b0e38daa632c0856dea
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-02-01 14:10:50 +0530

    Ref works in Class Component correctly

--- src/containers/App/Pages/Classes/AddEditDepartmentFormModal.jsx
+++ src/containers/App/Pages/Classes/AddEditDepartmentFormModal.jsx
@@ -1,44 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Modal, Form, Input } from 'antd';
 
-const AddEditDepartmentFormModal = ({
-  visible, onCancel, onCreate, form,
-}) => {
-  const { getFieldDecorator } = form;
+class AddEditDepartmentFormModal extends Component {
+  render() {
+    const {
+      visible, onCancel, onCreate, form,
+    } = this.props;
 
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 6 },
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 18 },
-    },
-  };
+    const { getFieldDecorator } = form;
 
-  return (
-    <Modal
-      title="Add Department"
-      okText="Submit"
-      visible={visible}
-      onCancel={onCancel}
-      onOk={onCreate}
-    >
-      <Form {...formItemLayout}>
-        <Form.Item label="Code">
-          {getFieldDecorator('code', {
-            rules: [{ required: true, message: 'Please input the department code!' }],
-          })(<Input />)}
-        </Form.Item>
-        <Form.Item label="Name">
-          {getFieldDecorator('name', {
-            rules: [{ required: true, message: 'Please input the department name!' }],
-          })(<Input type="textarea" />)}
-        </Form.Item>
-      </Form>
-    </Modal>
-  );
-};
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 18 },
+      },
+    };
+
+    return (
+      <Modal
+        title="Add Department"
+        okText="Submit"
+        visible={visible}
+        onCancel={onCancel}
+        onOk={onCreate}
+      >
+        <Form {...formItemLayout}>
+          <Form.Item label="Code">
+            {getFieldDecorator('code', {
+              rules: [{ required: true, message: 'Please input the department code!' }],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item label="Name">
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: 'Please input the department name!' }],
+            })(<Input type="textarea" />)}
+          </Form.Item>
+        </Form>
+      </Modal>
+    );
+  }
+}
 
 export default Form.create({ name: 'form_in_modal' })(AddEditDepartmentFormModal);
