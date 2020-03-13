ac770dd9161885c19ffadae319226f3601e93202
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-03-13 23:55:12 +0530

    Added Footer

--- src/containers/Evaluation/Form/index.jsx
+++ src/containers/Evaluation/Form/index.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Row, Col, Divider } from 'antd';
+import { Row, Col, Divider, Layout } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import FeedbackFormHeader from './FeedbackFormHeader';
 import FeedbackFormBody from './FeedbackFormBody';
 
+const { Content, Footer } = Layout;
+
 const FeedbackFormPage = ({ loggedInUserInfo }) => (
-  <Row className="account">
-    <Col xs={{ span: 22, offset: 1 }} sm={{ span: 16, offset: 4 }} className="account__wrapper">
-      <div className="account__card evaluation__form__card">
-        <FeedbackFormHeader userName={loggedInUserInfo.userName} />
-        <Divider />
-        <FeedbackFormBody loggedInUserInfo={loggedInUserInfo} />
-      </div>
-    </Col>
-  </Row>
+  <Layout className="layout">
+    <Content>
+      <Row className="account">
+        <Col xs={{ span: 22, offset: 1 }} sm={{ span: 16, offset: 4 }} className="account__wrapper">
+          <div className="account__card evaluation__form__card">
+            <FeedbackFormHeader userName={loggedInUserInfo.userName} />
+            <Divider />
+            <FeedbackFormBody loggedInUserInfo={loggedInUserInfo} />
+          </div>
+        </Col>
+      </Row>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}><b>Made with <span role="img" aria-label="love">&#128150;</span> in India by LifeCOMPILERS</b></Footer>
+  </Layout>
 );
 
 FeedbackFormPage.propTypes = {
