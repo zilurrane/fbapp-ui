d62a3a86285cc2ec15dfeb632c56db50957df821
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-03-14 00:27:43 +0530

    Ugly User Icon

--- src/containers/Evaluation/Form/index.jsx
+++ src/containers/Evaluation/Form/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Row, Col, Divider, Layout } from 'antd';
+import { Row, Col, Divider, Layout, Dropdown, Menu, Icon } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import FeedbackFormHeader from './FeedbackFormHeader';
 import FeedbackFormBody from './FeedbackFormBody';
 
 const { Content, Footer } = Layout;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="1">
+      <Icon type="user" />
+      1st menu item
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Icon type="user" />
+      2nd menu item
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Icon type="user" />
+      3rd item
+    </Menu.Item>
+  </Menu>
+);
 
 const FeedbackFormPage = ({ loggedInUserInfo }) => (
   <Layout className="layout">
@@ -14,6 +31,9 @@ const FeedbackFormPage = ({ loggedInUserInfo }) => (
         <Col xs={{ span: 22, offset: 1 }} sm={{ span: 16, offset: 4 }} className="account__wrapper">
           <div className="account__card evaluation__form__card">
             <FeedbackFormHeader userName={loggedInUserInfo.userName} />
+            <Dropdown.Button type="link" overlay={menu} className="float-right" trigger={['click']} icon={<Icon type="user" />}>
+              { loggedInUserInfo.userName }
+            </Dropdown.Button>
             <Divider />
             <FeedbackFormBody loggedInUserInfo={loggedInUserInfo} />
           </div>
