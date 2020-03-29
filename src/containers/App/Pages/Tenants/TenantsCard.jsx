5737c9d6c7d82a55fd5fd5938017103e0cbcfb79
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-03-29 16:34:33 +0530

    Added antd table for tenants

--- src/containers/App/Pages/Tenants/TenantsCard.jsx
+++ src/containers/App/Pages/Tenants/TenantsCard.jsx
@@ -1,18 +1,81 @@
-import React from 'react';
-import { Card, CardBody, Col } from 'reactstrap';
+import React, { Fragment, Component } from 'react';
+import { Card, CardBody, Row, Col } from 'reactstrap';
+import { Table, Button } from 'antd';
 
-const TenantsCard = () => (
-  <Col md={12}>
-    <Card>
-      <CardBody>
-        <div className="card__title">
-          <h5 className="bold-text">Tenants title</h5>
-          <h5 className="subhead">Tenants subhead</h5>
-        </div>
-        <p>Your content here</p>
-      </CardBody>
-    </Card>
-  </Col>
-);
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
+
+class TenantsCard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Col md={12}>
+          <Row>
+            <Col md={12}>
+              <Col md={12}>
+                <Card>
+                  <CardBody>
+                    <Row>
+                      <Col>
+                        <div className="card__title">
+                          <h5 className="bold-text">Tenants title</h5>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <div>
+                          <div className="card__title">
+                            <h5 className="bold-text">&nbsp;</h5>
+                            <Button className="card__actions" type="primary">
+                              Add
+                            </Button>
+                          </div>
+                          <Table columns={columns} dataSource={data} size="middle" bordered />
+                        </div>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Col>
+          </Row>
+        </Col>
+      </Fragment>
+    );
+  }
+}
 
 export default TenantsCard;
