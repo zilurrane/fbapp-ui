c48335dcf4f40dc9a194d82659cf095fb0760d2e
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-09-13 12:36:36 +0530

    Add edit class icon

--- src/containers/App/Pages/Classes/ClassesTable.jsx
+++ src/containers/App/Pages/Classes/ClassesTable.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Table } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Button } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 import { getAllClassesByDepartmentCode } from '../../../../redux/actions/departmentActions';
 
 class ClassesTable extends Component {
@@ -18,12 +20,19 @@ class ClassesTable extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Table size="sm" hover striped>
+      <Table size="sm" hover striped responsive>
+        <colgroup>
+          <col style={{ width: '5%' }} />
+          <col style={{ width: '35%' }} />
+          <col style={{ width: '50%' }} />
+          <col style={{ width: '10%' }} />
+        </colgroup>
         <thead>
           <tr>
             <th>#</th>
             <th>Code</th>
             <th>Name</th>
+            <th className="text-center">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -33,13 +42,16 @@ class ClassesTable extends Component {
                 <th scope="row">{index + 1}</th>
                 <td>{d.code}</td>
                 <td>{d.name}</td>
+                <td className="text-center">
+                  <Button size="small" type="primary" icon={<EditOutlined />} />
+                </td>
               </tr>
             ))
           }
           {
             classes.length === 0 &&
             <tr>
-              <td colSpan="3">No classes found!</td>
+              <td colSpan="4">No classes found!</td>
             </tr>
           }
         </tbody>
