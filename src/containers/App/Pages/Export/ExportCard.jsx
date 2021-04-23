741038a0f6b0c59b6cf1386c5483f277868b5b95
zilurrane <zilurrane@gmail.com>
2021-04-23 20:19:49 +0530

    Download feedback in excel format

--- src/containers/App/Pages/Export/ExportCard.jsx
+++ src/containers/App/Pages/Export/ExportCard.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import { Card, CardBody, Col } from 'reactstrap';
 import { Tabs, Button } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
+import { saveAs } from 'file-saver';
+import { connect } from 'react-redux';
+import { callUnAuthApi } from '../../../../shared/helpers/fetch-helper';
 
 const { TabPane } = Tabs;
 
-const ExportCard = () => (
+const downloadExcel = (tenantId) => {
+  let filename;
+  callUnAuthApi('https://fbapp-report-api.herokuapp.com/report/feedback', { method: 'GET', headers: { TenantId: tenantId } })
+    .then((response) => {
+      if (response.status === 200) {
+        filename = response.headers.get('content-disposition');
+        // eslint-disable-next-line prefer-destructuring
+        filename = filename.split('filename=')[1];
+        return response.blob();
+      }
+      return new Promise();
+    })
+    .then((body) => {
+      saveAs(body, filename, { type: 'application/octet-stream' });
+    });
+};
+
+const ExportCard = ({ tenantId }) => (
   <Col md={12}>
     <Card>
       <CardBody>
         <Tabs defaultActiveKey="1">
           <TabPane tab="Full School" key="1">
-            <Button type="primary" icon={<DownloadOutlined />}>
+            <Button onClick={() => downloadExcel(tenantId)} type="primary" icon={<DownloadOutlined />}>
               Feedback Excel
             </Button>
           </TabPane>
@@ -27,4 +47,5 @@ const ExportCard = () => (
   </Col>
 );
 
-export default ExportCard;
+const mapStateToProps = state => ({ tenantId: (state.tenant.selectedTenant || {})._id });
+export default connect(mapStateToProps, null)(ExportCard);
